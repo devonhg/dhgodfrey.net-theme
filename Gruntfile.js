4efe723b22bb4ff944658ecf0df94ecd4cfe6c9b
devonhg <devon.hugh.godfrey@gmail.com>
2015-05-21 15:42:44 -0400

    Fixed Grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,27 @@
 module.exports = function(grunt) {
-	// Project configuration.
-		grunt.initConfig({
-				pkg: grunt.file.readJSON(‘package.json’),
-			uglify: {
-			options: {
-				banner: ‘/*! <%= pkg.name %> <%= grunt.template.today(“yyyy-mm-dd”) %> */\n’
-			},
-			build: {
-				src: ‘src/<%= pkg.name %>.js’,
-					dest: ‘build/<%= pkg.name %>.min.js’
-				}
-			}
-		});
-		
-	// Load the plugin that provides the “uglify” task.
-	grunt.loadNpmTasks(‘grunt-contrib-uglify’);
-	// Default task(s).
-	grunt.registerTask(‘default’, [‘uglify’]);
-}
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    }
+  });
+  
+  grunt.registerTask('speak', function(){
+	console.log("I'm speaking");
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
